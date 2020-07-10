a1924c97fab5d63a6b81051c8ea15e27d942f8f3
gfoza <gfozza@hotmail.com>
2020-07-09 22:20:20 -0300

    codigo da aula

---
+++ main.js
@@ -0,0 +1,48 @@
+class Pessoa {
+    constructor(nome, altura, peso) {
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
+
+class Vingador extends Pessoa {
+    constructor(nome,altura, peso, poderes, temArmadura, isMembroFundador) {
+        super(nome, altura, peso);
+        this.poderes = poderes;
+        this.temArmadura = temArmadura;
+        this.isMembroFundador = isMembroFundador; 
+        alert(this.nome);
+    }
+
+    lutar() {
+        alert(`${this.nome} que tem altura de ${this.altura}m, peso de ${this.peso} está lutando utilizando ${this.poderes[1]}`)
+    }
+}
+
+const thor = new Vingador(
+    "Thor", 
+    1.85, 
+    "100kg",
+    ["Super Força", "Raio"],
+    true, 
+    true
+);
+
+document.getElementById("lutar").onclick = function () {
+    thor.lutar();
+}
+
+class operacoesMatematicas {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+        }
+
+        static soma(a, b) {
+            return this.a + this.b;
+        }
+}
+
+const sum = operacoesMatematicas.soma(4, 7);
+alert(sum);
